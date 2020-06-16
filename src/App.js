248abe8e015a23beb9c8ea76e19bba833f75a486
Andrea Habib <andrea.atef@yahoo.com>
2020-06-16 11:10:05 -0400

    adjusted navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import { Header } from "./components/layout";
 import "./App.css";
 
 export class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
-export class header extends Component {
+export class Header extends Component {
   render() {
     return (
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
-          Navbar
+          GameINFO
         </a>
         <button
           class="navbar-toggler"
@@ -28,37 +28,12 @@ export class header extends Component {
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
-                Link
+                Login
               </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled" href="#">
-                Disabled
+              <a class="nav-link" href="#">
+                Register
               </a>
             </li>
           </ul>
@@ -66,7 +41,7 @@ export class header extends Component {
             <input
               class="form-control mr-sm-2"
               type="search"
-              placeholder="Search"
+              placeholder="Search for a game here!"
               aria-label="Search"
             />
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
@@ -79,4 +54,4 @@ export class header extends Component {
   }
 }
 
-export default header;
+export default Header;
