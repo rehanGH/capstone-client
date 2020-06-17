d1f897303308bf4636fc2fccfca29ff8ea365d6b
Neissa Dorsinville <neissa@dorsinville.com>
2020-06-17 11:29:11 -0400

    Completed games container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import GamesContainer from './components';
+
 export class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return(
+      <div className="App">
+        <GamesContainer />
+      </div>
+    );
   }
 }
 
--- src/components/GamesContainer.js
+++ src/components/GamesContainer.js
@@ -1,38 +1,40 @@
-// import React from 'react';
-// import React, { useEffect } from 'react'
-// import { connect } from 'react-redux '
-// import fetchGames from '../redux/utilities/containers/AllGames/AllGamesActions';
-//
-// function GameContainer({ fetchGames }) {
-//   useEffect(() => {
-//     fetchGames()
-//   }, [])
-//   return gamesData.loading ? (
-//     <h2> Loading <h2>
-//   ) : gamesData.error ? (
-//     <h2> {gamesData.error} </h2>
-//   ) : (
-//     <div className="games-grid">
-//       <h2> Trending Games <h2>
-//         <div>
-//           {
-//             gamesData && gamesData.games && gamesData.users.map(game => <p> {game.name}</p>)
-//           }
-//         </div>
-//     </div>
-//   )
-// }
-//
-// const mapStateToProps = state => {
-//   return {
-//     gamesData: state.games
-//   }
-// }
-//
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     fetchGames: () dispatch(fetchGames())
-//   }
-// }
-//
-// export default connect(mapStateToProps, mapDispatchToProps)(GamesContainer);
+import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux ';
+import fetchGames from '../redux/utilities/containers/AllGames/AllGamesActions';
+
+function GameContainer({ fetchGames, gamesData }) {
+  useEffect(() => {
+    fetchGames()
+  }, [])
+
+  return gamesData.loading ? (
+    <h2> Loading </h2>
+  ) : gamesData.error ? (
+    <h2> {gamesData.error} </h2>
+  ) : (
+    <div className="games-grid">
+      <h2> Trending Games </h2>
+        <div>
+          {
+            gamesData && gamesData.games && gamesData.games.map(game => <p> {game.name}</p>)
+          }
+        </div>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    gamesData: state.games
+  }
+}
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchGames: () dispatch(fetchGames())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GamesContainer);
